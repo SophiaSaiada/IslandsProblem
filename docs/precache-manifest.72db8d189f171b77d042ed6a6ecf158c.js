1746d6a358425b70ebe57269e4472a2dcefb6282
Sophia Saiada <sophia.saiada@gmail.com>
2020-02-08 16:55:29 +0200

    Add github pages preview

---
+++ docs/precache-manifest.72db8d189f171b77d042ed6a6ecf158c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2570b7fc12425958d6a473d7ba9d7bf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b4cfd5641271fc16cfb",
+    "url": "/static/css/main.c3d7cf78.chunk.css"
+  },
+  {
+    "revision": "fa89e3f078e4c42202f7",
+    "url": "/static/js/2.cd14a78f.chunk.js"
+  },
+  {
+    "revision": "9c7e6cca0df3be74c612f63119a9d27d",
+    "url": "/static/js/2.cd14a78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b4cfd5641271fc16cfb",
+    "url": "/static/js/main.55d4bb2a.chunk.js"
+  },
+  {
+    "revision": "f4c79ec2e53920247735",
+    "url": "/static/js/runtime-main.9da123f4.js"
+  },
+  {
+    "revision": "14861c271e2b0607bc68b07aa9f6dca0",
+    "url": "/static/media/jetbrainsmono-bold-webfont.14861c27.woff"
+  },
+  {
+    "revision": "4afee8c497cf71f0049d22ee510bfbf9",
+    "url": "/static/media/jetbrainsmono-bold-webfont.4afee8c4.woff2"
+  },
+  {
+    "revision": "197c69e622701eb3fe8906a699191aee",
+    "url": "/static/media/jetbrainsmono-regular-webfont.197c69e6.woff"
+  },
+  {
+    "revision": "2b149b81d114a8f0b09c83f029ea60a0",
+    "url": "/static/media/jetbrainsmono-regular-webfont.2b149b81.woff2"
+  }
+]);
