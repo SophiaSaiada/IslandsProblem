3fde36e59098002bef704f89ab99510f3e3a0590
Sophia Saiada <sophia.saiada@gmail.com>
2020-02-08 16:58:44 +0200

    Update docs

---
+++ docs/precache-manifest.2f6bcc301aa25d3f9b537722d81ca525.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1abafff5ea1c3978add97854f0ce444",
+    "url": "/IslandsProblem/index.html"
+  },
+  {
+    "revision": "cfac426961c4bce5d322",
+    "url": "/IslandsProblem/static/css/main.7e05891e.chunk.css"
+  },
+  {
+    "revision": "fa89e3f078e4c42202f7",
+    "url": "/IslandsProblem/static/js/2.cd14a78f.chunk.js"
+  },
+  {
+    "revision": "9c7e6cca0df3be74c612f63119a9d27d",
+    "url": "/IslandsProblem/static/js/2.cd14a78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfac426961c4bce5d322",
+    "url": "/IslandsProblem/static/js/main.e7bc4eb2.chunk.js"
+  },
+  {
+    "revision": "3046c76c6d7999ead579",
+    "url": "/IslandsProblem/static/js/runtime-main.22bf6b9f.js"
+  },
+  {
+    "revision": "14861c271e2b0607bc68b07aa9f6dca0",
+    "url": "/IslandsProblem/static/media/jetbrainsmono-bold-webfont.14861c27.woff"
+  },
+  {
+    "revision": "4afee8c497cf71f0049d22ee510bfbf9",
+    "url": "/IslandsProblem/static/media/jetbrainsmono-bold-webfont.4afee8c4.woff2"
+  },
+  {
+    "revision": "197c69e622701eb3fe8906a699191aee",
+    "url": "/IslandsProblem/static/media/jetbrainsmono-regular-webfont.197c69e6.woff"
+  },
+  {
+    "revision": "2b149b81d114a8f0b09c83f029ea60a0",
+    "url": "/IslandsProblem/static/media/jetbrainsmono-regular-webfont.2b149b81.woff2"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72db8d189f171b77d042ed6a6ecf158c.js"
+  "/IslandsProblem/precache-manifest.2f6bcc301aa25d3f9b537722d81ca525.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IslandsProblem/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
